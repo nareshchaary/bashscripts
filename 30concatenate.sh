d02971a6c6cffea3c027ffb280b845779617635a
nareshchaary <nareshchilumula2930@gmail.com>
2023-03-09 15:02:11 +0530

    Substring and concatenate

---
+++ 30concatenate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash  
+#Script to Concatenate Strings  
+#Declaring the first String   
+str1="We welcome you" 
+#Declaring the Second String  
+str2=" on Javatpoint."    
+#Combining first and second string  
+str3="$str1$str2"    
+#Printing a new string by combining both   
+echo $str3 
