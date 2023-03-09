f663eed9a2d247a2084cdf278c2dc391cedea2fc
nareshchaary <nareshchilumula2930@gmail.com>
2023-03-09 15:03:24 +0530

    bash function

---
+++ 31bash_function.sh
@@ -0,0 +1,12 @@
+#!/bin/bash  
+#Script to pass and access arguments  
+function_arguments()  
+    {  
+    echo $1  
+    echo $2  
+    echo $3  
+    echo $4  
+    echo $5  
+    }  
+#Calling function_arguments  
+function_arguments "We""welcome""you""on""Antra." 
